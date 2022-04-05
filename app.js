f6cd6b653b8f4f5d4e06495f5c9fabe043b7da4c
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-04-05 01:03:52 +0000

    message

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+window.onload = function () {
+    var top = document.querySelector(".topBox")
+    var numb = document.querySelector(".number")
+    var bottom = document.querySelector(".bottomBox")
+    var button = document.querySelector(".btn")
+    const sign = ['♦', '♥', '♠', '♣',];
+    const value = ['1','2','3','4','5','6','7','8','9','J','Q','K'];
+    const color = ['black', 'red'];
+    let currentSign = sign[Math.floor(Math.random()*sign.length -1)]
+    let currentValue = value[Math.floor(Math.random()*value.length -1)]
+    
+    button.addEventListener("click", function(){
+        if (currentSign ==='♦' || currentSign === '♥') {
+    top.style.color = color[1];
+    bottom.style.color = color[1];
+        } else {
+            top.style.color = color[0];
+            bottom.style.color = color[0];
+        }
+        numb.innerHTML = currentValue;
+        top.innerHTML = currentSign;
+        bottom.innerHTML = currentSign;     
+    
+    })
+}
